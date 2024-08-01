f862f9c48591cad4e79832531512fe90dc337785
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 12:22:04 +0200

    feat: declare rod as a dictionaie, Give it the strings 'A', 'B', and 'C' as keys and set each of them to an empty list

---
+++ lecture.py
@@ -0,0 +1,23 @@
+# Tower of Hanoi Puzzle
+
+# Step 1
+
+# In this project, you will solve the mathematical puzzle known as the Tower of Hanoi. 
+# The puzzle consists of three rods and a number of disks of different diameters.
+
+# The goal of this puzzle is moving the disks from the first rod to the third rod, following specific 
+# rules that restrict placing a larger disk on top of a smaller one.
+
+# Start by creating an empty dictionary named rods to represent the rods.
+
+rods = {}
+
+# Step 2
+
+# The rods dictionary will represent the three rods with their disks. 
+# Give it the strings 'A', 'B', and 'C' as keys and set each of them to an empty list.
+
+rods = {'A': [], 'B': [], 'C': []}
+
+
+
