ea20f56f6aab459429b4fc103a68f2fbc2944946
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 12:23:24 +0200

    feat: The puzzle starts with the disks piled up on the first rod, in decreasing size. Need to populate your first list with numbers representing the various disk sizes. Instead of adding the items manually to the first list, generate a sequence of numbers from 3 to 1 # by using the range() function and assign it to rods['A']

--- lecture.py
+++ lecture.py
@@ -19,5 +19,30 @@ rods = {}
 
 rods = {'A': [], 'B': [], 'C': []}
 
+# Step 3
+
+# The puzzle starts with the disks piled up on the first rod, in decreasing size. 
+# You need to populate your first list with numbers representing the various disk sizes.
+
+# Instead of adding the items manually to the first list, generate a sequence of numbers from 3 to 1 
+# by using the range() function and assign it to rods['A'].
+
+# The syntax is range(x, y, h), where x is the starting integer (inclusive), y is the last integer (not inclusive), 
+# and h is the difference between a number and the next one in the sequence.
+
+rods = {
+    'A': range(3,0,-1),
+    'B': [],
+    'C': []
+}
+
+# Step 4
+
+# Now check the data type of your 'A' key by passing it to the type() function and print the result on the terminal.
+
+print(type(rods['A']))
+
+
+
 
 
